7db3d5ff24935ecb5d1b7993b96ba317b12557c0
David Andrea <davejandrea@gmail.com>
2022-10-21 20:28:02 -0300

    1.3 checkpoint

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -1,18 +1,23 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2}
-                     exercises3={exercises3}/>
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+            <Content part1={part1} part2={part2} part3={part3} />
+            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
         </div>
     )
 }
@@ -23,26 +28,26 @@ const Header = ({course}) => {
     return <h1>{course}</h1>
 }
 
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+const Content = ({part1, part2, part3}) => {
     return (
         <div>
             <Part
-                part={part1} exercises={exercises1}
+                name={part1.name} exercises={part1.exercises}
             />
             <Part
-                part={part2} exercises={exercises2}
+                name={part2.name} exercises={part2.exercises}
             />
             <Part
-                part={part3} exercises={exercises3}
+                name={part3.name} exercises={part3.exercises}
             />
         </div>
     )
 }
 
-const Part = ({part, exercises}) => {
+const Part = ({name, exercises}) => {
     return (
         <p>
-            {part} {exercises}
+            {name} {exercises}
         </p>
     )
 }
