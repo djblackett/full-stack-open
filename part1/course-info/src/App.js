0ab6c6268c42697c64ca19e78e1867f04ab2fcf1
David Andrea <davejandrea@gmail.com>
2022-10-21 16:11:42 -0300

    refactor Content with Part components

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -25,7 +25,7 @@ const Header = ({course}) => {
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
     return (
-        <>
+        <div>
             <Part
                 part={part1} exercises={exercises1}
             />
@@ -35,7 +35,7 @@ const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
             <Part
                 part={part3} exercises={exercises3}
             />
-        </>
+        </div>
     )
 }
 
