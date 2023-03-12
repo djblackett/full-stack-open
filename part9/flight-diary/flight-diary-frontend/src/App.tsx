636149a42040ab569e02125c21cb31c45e13bd66
djblackett <49618491+djblackett@users.noreply.github.com>
2023-03-12 17:24:28 -0300

    finished 9.19

--- part9/flight-diary/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry } from "../types";
 import { v1 as uuid } from "uuid";
 
 function App() {
@@ -43,11 +43,25 @@ function App() {
   };
 
   const resetFields = () => {
-    setVisibility("");
+    // setVisibility("");
     setDate("");
-    setWeather("");
+    // setWeather("");
     setComment("");
+    clearRadioButtons();
+
+    const date = document.querySelector("input[type=date]") as HTMLInputElement;
+    date.valueAsDate = new Date();
   };
+
+  function clearRadioButtons() {
+    const ele = document.querySelectorAll(
+      "input[type=radio]"
+    ) as NodeListOf<HTMLInputElement>;
+    console.log(ele);
+    for (let i = 0; i < ele.length; i++) {
+      ele[i].checked = false;
+    }
+  }
 
   useEffect(() => {
     const getDiaries = async () => {
@@ -76,35 +90,114 @@ function App() {
           alignItems: "flex-end",
         }}
       >
-        <div style={{ display: "grid", gridTemplate: "1fr / 1fr 1fr" }}>
-          <label style={{ justifySelf: "flex-start" }}>Date</label>{" "}
+        <div
+          style={{
+            display: "grid",
+            gridTemplate: "1fr / 1fr 2fr",
+            width: "100%",
+          }}
+        >
+          <label style={{ justifySelf: "center" }} htmlFor="date">
+            Date
+          </label>
           <input
-            type="text"
+            type="date"
             name="date"
             value={date}
             onChange={(e) => setDate(e.target.value)}
           />
         </div>
-        <div style={{ display: "grid", gridTemplate: "1fr / 1fr 1fr" }}>
-          <label style={{ justifySelf: "flex-start" }}>Visibility</label>
-          <input
-            type="text"
-            name="visibility"
-            value={visibility}
-            onChange={(e) => setVisibility(e.target.value)}
-          />
+        <div
+          style={{
+            display: "grid",
+            gridTemplate: "1fr / 1fr 2fr",
+            width: "100%",
+          }}
+        >
+          <label style={{ justifySelf: "center" }} htmlFor="visibility">
+            Visibility
+          </label>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <label>Great</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="great"
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+            <label>Good</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="good"
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+            <label>Ok</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="ok"
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+            <label>Poor</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="poor"
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+          </div>
         </div>
-        <div style={{ display: "grid", gridTemplate: "1fr / 1fr 1fr" }}>
-          <label style={{ justifySelf: "flex-start" }}>Weather</label>
-          <input
-            type="text"
-            name="weather"
-            value={weather}
-            onChange={(e) => setWeather(e.target.value)}
-          />
+        <div style={{ display: "grid", gridTemplate: "1fr / 1fr 2fr" }}>
+          <label style={{ justifySelf: "center" }} htmlFor="weather">
+            Weather
+          </label>
+          <div>
+            <label>Sunny </label>
+            <input
+              type="radio"
+              name="weather"
+              value={weather}
+              onChange={(e) => setWeather(e.target.value)}
+            />
+            <label>Rainy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="rainy"
+              onChange={(e) => setWeather(e.target.value)}
+            />
+            <label>Cloudy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="cloudy"
+              onChange={(e) => setWeather(e.target.value)}
+            />
+            <label>Stormy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="stormy"
+              onChange={(e) => setWeather(e.target.value)}
+            />
+            <label>Windy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="windy"
+              onChange={(e) => setWeather(e.target.value)}
+            />
+          </div>
         </div>
-        <div style={{ display: "grid", gridTemplate: "1fr / 1fr 1fr" }}>
-          <label style={{ justifySelf: "flex-start" }}>Comment</label>
+        <div
+          style={{
+            display: "grid",
+            gridTemplate: "1fr / 1fr 2fr",
+            width: "100%",
+          }}
+        >
+          <label style={{ justifySelf: "center" }}>Comment</label>
           <input
             type="text"
             name="comment"
@@ -120,7 +213,7 @@ function App() {
       <h1>Diaries</h1>
       {diaries &&
         diaries.map((diary) => (
-          <div style={{ marginBottom: "1rem" }} key={diary.date}>
+          <div style={{ marginBottom: "1rem" }} key={diary.id}>
             <p>
               <em>{diary.date}</em>
             </p>
--- part9/flight-diary/flight-diary-frontend/types.ts
+++ part9/flight-diary/flight-diary-frontend/types.ts
@@ -1,4 +1,12 @@
+export interface NewDiaryEntry {
+  date: string;
+  visibility: string;
+  weather: string;
+  comment: string;
+}
+
 export interface DiaryEntry {
+  id: number;
   date: string;
   visibility: string;
   weather: string;
